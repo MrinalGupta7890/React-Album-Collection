cdd17df47222a36861d7ba3e6f765a00a1abb417
Mrunal Gupta <125861423+MrinalGupta7890@users.noreply.github.com>
2024-04-20 16:20:58 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,19 @@
+// Import necessary modules
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-//get button name and path for evaery component
+// Define the Navbar component as a functional component that takes in props as an argument
 const Navbar = (props) => {
 
+  // Return the JSX structure for the Navbar component
   return (
     <div className='navbar'>
       <h2>ALBUMS</h2>
+      {/* When the button is clicked, navigate to the path specified in props.path */}
       <Link to={props.path}><button>{props.page}</button></Link>
     </div>
   )
 }
 
+// Export the Navbar component as the default export
 export default Navbar
