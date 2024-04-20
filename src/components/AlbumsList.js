1bfaba7dc022e60da60d2c55f7b83850330dfcbb
Mrunal Gupta <125861423+MrinalGupta7890@users.noreply.github.com>
2024-04-20 16:19:01 +0530

    Update AlbumsList.js

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,20 +1,27 @@
+// Import necessary modules
 import React from 'react'
 import List from './List'
 import Navbar from './Navbar'
 
 
-//get all albums list from app and then call list component for each list on the albums
+// Define the AlbumsList component as a functional component that takes in props as an argument
 const AlbumsList = (props) => {
+
+  // Return the JSX structure for the AlbumsList component
   return (
     <>
-      {/* Navbar */}
+      {/* Include the Navbar component at the top of the page */}
       <Navbar page="Add Album" path="/add-album" />
 
+      {/* Create a div element with a class of 'albums-list' */}
       <div className='albums-list'>
+
+        {/* Use the map function to iterate over the albums array in props and return a List component for each album */}
         {props.albums.map((album) => <List album={album} key={album.id} setUpdateAlbum={props.setUpdateAlbum} deleteAlbumFromList={props.deleteAlbumFromList} />)}
       </div>
     </>
   )
 }
 
+// Export the AlbumsList component as the default export
 export default AlbumsList
