e0f2b5d563263a29d2465c7591bb1f9810aeb479
Mrunal Gupta <125861423+MrinalGupta7890@users.noreply.github.com>
2024-04-20 16:22:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+// Import necessary modules
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './components/App';
 
+// Create a root element for the React application
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component inside a BrowserRouter and React.StrictMode
 root.render(
   <BrowserRouter>
     <React.StrictMode>
