a7a375879998478beae32eb38da7f0a9cc665f5b
Mrunal Gupta <125861423+MrinalGupta7890@users.noreply.github.com>
2024-04-20 16:19:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
+// Import necessary modules
 import React, { Component } from 'react'
 import { Routes, Route } from "react-router-dom";
 import AddAlbum from './AddAlbum';
 import AlbumsList from './AlbumsList';
 import UpdateAlbum from './UpdateAlbum';
 
+// Define the App component as a class component that extends Component
 export default class App extends Component {
+  // Initialize the state of the component with an empty array for albums and an empty object for updateAlbum
   constructor() {
     super();
     this.state = {
@@ -13,37 +16,40 @@ export default class App extends Component {
     }
   }
 
-  // this function call first time when app render
+  // This function is called when the component is first rendered
+  // It fetches the albums from the JSONPlaceholder API and sets the state of the component with the fetched albums
   componentDidMount = async () => {
     const albums = await fetch('https://jsonplaceholder.typicode.com/albums')
-      .then((response) => response.json())
-      .then((json) => json);
+     .then((response) => response.json())
+     .then((json) => json);
     this.setState({
       albums
     })
   }
 
-
-  //delete album function-
-  //this function take album id from albums list and then delete the album from albums list and update state
+  // This function is called when the user wants to delete an album
+  // It takes the album ID as an argument, fetches the album from the JSONPlaceholder API, and deletes it
+  // It then updates the state of the component with the new list of albums
   deleteAlbumFromList = (id) => {
     fetch(`https://jsonplaceholder.typicode.com/albums/${id}`, { method: 'DELETE', })
-    const newAlbums = this.state.albums.filter((album) => album.id !== id);
+    const newAlbums = this.state.albums.filter((album) => album.id!== id);
     alert("Your Album Deleted successfully");
     this.setState({
       albums: newAlbums,
     })
   }
 
-
-  //update album functions-
-  //this function take album object from albums list and set state for update album
+  // This function is called when the user wants to update an album
+  // It takes the album object as an argument and sets the state of the component with the updated album
   setUpdateAlbum = async (album) => {
     this.setState({
       updateAlbum: album
     })
   }
-  //this function take album id, updateTitle, updateUserid, oldAlbum and then update and set state 
+
+  // This function is called when the user wants to update an album
+  // It takes the album ID, updated title, updated user ID, and the old album object as arguments
+  // It fetches the album from the JSONPlaceholder API, updates it with the new title and user ID, and sets the state of the component with the updated album
   updateAlbumInList = async (id, updateTitle, updateUserid, oldAlbum) => {
     const albums = this.state.albums;
     const index = albums.indexOf(oldAlbum);
@@ -74,9 +80,9 @@ export default class App extends Component {
     alert("Update Successfully done")
   }
 
-
-  //add album function-
-  //this function take userid and title from input and then added to the bottom of the albums list
+  // This function is called when the user wants to add an album
+  // It takes the user ID and title as arguments, fetches the albums from the JSONPlaceholder API, and adds the new album to the list
+  // It then sets the state of the component with the new list of albums
   addAlbumToList = (userId, title) => {
     fetch('https://jsonplaceholder.typicode.com/albums', {
       method: 'POST',
@@ -102,11 +108,15 @@ export default class App extends Component {
     alert("New Album added successfully in the bottom");
   }
 
-
+  // This function is called when the component is rendered
+  // It returns a JSX structure that includes a Routes component with three Route components
+  // The first Route component renders the AlbumsList component with the albums, setUpdateAlbum, and deleteAlbumFromList props
+  // The second Route component renders the AddAlbum component with the addAlbumToList prop
+  // The third Route component renders the UpdateAlbum component with the album and updateAlbumInList props
   render() {
     return (
       <>
-        <Routes>
+       <Routes>
           <Route path='/' element={<AlbumsList albums={this.state.albums} setUpdateAlbum={this.setUpdateAlbum} deleteAlbumFromList={this.deleteAlbumFromList} />}></Route>
           <Route path='/add-album' element={<AddAlbum addAlbumToList={this.addAlbumToList} />}></Route>
           <Route path='/update-album' element={<UpdateAlbum album={this.state.updateAlbum} updateAlbumInList={this.updateAlbumInList} />}></Route>
